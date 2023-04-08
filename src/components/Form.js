9b59360fbf1c6b6cec7d6c7afc1455c73391c2f2
aondrej ryan <aondrejryan11@gmail.com>
2023-04-08 15:27:20 -0700

    added password validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,14 +3,29 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 function Form(){
     //how ot write a form in react
+    const number = [1,2,3,4,5,6,7,8,9,0].map(x => x.toString())
     const passwordInputRef = React.useRef()
-    const handleSubmit = (event) => {
+    const [password, setPassword] = React.useState('')
 
+    // checking if passowrd contains a number 
+    let hasNumber = false
+    let error = true
+
+    //looping throgh password to check if it has a number 
+    password.split('').forEach(x => {
+        if(number.includes(x) && password.length >= 8) hasNumber = true
+    })
+
+    error = hasNumber ? null : 'passworns needs at least 1 number'
+
+    const handleSubmit = (event) => {
         event.preventDefault()
-        // console.log('submitted')
          console.dir(event.target.elements.inputPassword2.value)
         const password = passwordInputRef.current.value
         console.log(password)
+    }
+    const handleChange = (event) => {
+        setPassword(event.target.value)
     }
     return (
         <Fragment>
@@ -22,10 +37,13 @@ function Form(){
             </div>
             <div className="col-auto">
                 <label htmlFor="inputPassword2" className="visually-hidden">Password</label>
-                <input type="text" ref={passwordInputRef} className="form-control" id="inputPassword2" placeholder="Password"/>
+                <input type="text" ref={passwordInputRef} onChange={handleChange} className="form-control" id="inputPassword2" placeholder="Password"/>
             </div>
+            <span style={{
+                color: 'red'
+            }}>{error}</span>
             <div className="col-auto">
-                <button type="submit" className="btn btn-primary mb-3">Confirm identity</button>
+                <button type="submit" disabled={Boolean(error)} className="btn btn-primary mb-3">Confirm identity</button>
             </div>
             </form>
         </Fragment>
