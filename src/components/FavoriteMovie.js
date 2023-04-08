150325f43209d206d1e0dd4a94597643d231c07f
aondrej ryan <aondrejryan11@gmail.com>
2023-04-08 01:39:53 -0700

    added a reusable hook

--- src/components/FavoriteMovie.js
+++ src/components/FavoriteMovie.js
@@ -1,13 +1,17 @@
 import React, {  Fragment } from 'react'
+// making a reuseable custom hook 
+    function useLocalStorageState(key, defaultvalue = ''){
+        const [state, setState] = React.useState(() => {
+        return   localStorage.getItem(key) || ''
+        })
+
+        React.useEffect(() => {
+            localStorage.setItem(key, state)
+            },[key,state])
+        return [state, setState]
+    }
 function FavoriteMovie(){
-    const [movieTitle, setMovieTitle] = React.useState(() => {
-        console.log('hey')
-      return   localStorage.getItem('favMovie') || ''
-    })
-    console.log('coolio')
-    React.useEffect(() => {
-        localStorage.setItem('favMovie', movieTitle)
-    })
+    const [movieTitle, setMovieTitle] = useLocalStorageState('favMovie')
     const handleChange = event => setMovieTitle(event.target.value)
     return (
         <>
